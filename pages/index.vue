20d8130250a91b6bb7f5e46379b9f5034f05b859
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-09-30 09:23:26 +0400

    fix servers

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,12 @@
     <common-header/>
     <common-share/>
     <index-primary-header/>
+    <ui-br height="20"/>
     <index-servers/>
     <ui-br height="10"/>
-    <index-statistics/>
-    <ui-br height="10"/>
     <index-latest-news/>
+    <ui-br height="10"/>
+    <index-statistics/>
     <ui-br height="10"/>
     <index-var-timeout/>
     <ui-br height="10"/>
