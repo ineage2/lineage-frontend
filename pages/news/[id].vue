fe37f4a3631ad9c5a270682992f9fcb43d1a0d4e
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-10-09 22:08:47 +0400

    Update [id].vue

--- pages/news/[id].vue
+++ pages/news/[id].vue
@@ -44,11 +44,25 @@ onMounted(() => {
         <common-loading v-if="loading"/>
         <transition name="fade">
             <div v-if="!loading">
-                <section v-for="news in newsList" :key="news.id" class="h-auto">
-                    <div class="w-dvw aspect-16-6 bg-fixed bg-no-repeat bg-cover bg-center"
-                         :style="{ backgroundImage: `url(${news.image_large_url})` }">
-                        
-                        
+                <section v-for="news in newsList" :key="news.id" class="news">
+                    <div class="news-inner">
+                        <div class="news-inner-background">
+                            <div class="news-inner-background-image vlada-transition"
+                                 :style="{ backgroundImage: `url(${news.image_large_url})` }"></div>
+                            <div class="news-inner-background-gradient news-inner-background-gradient-top"></div>
+                            <div class="news-inner-background-gradient news-inner-background-gradient-bottom"></div>
+                            <div class="news-inner-background-content">
+                                <div class="news-inner-background-content-title" data-aos="fade-up" data-aos-delay="100"
+                                     data-aos-duration="750" data-aos-once="true">
+                                    <h1>{{ news.title }}</h1>
+                                </div>
+                                <div class="news-inner-background-content-description" data-aos="fade-up"
+                                     data-aos-delay="300"
+                                     data-aos-duration="750" data-aos-once="true">
+                                    <h4>{{ news.description }}</h4>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <section class="max-w-7xl mx-auto md:top-[-200px] relative bg-slate-100 py-7 px-5 rounded-xl">
                         <h1 class="my-5 text-center">{{ news.title }}</h1>
@@ -61,7 +75,89 @@ onMounted(() => {
     </div>
 </template>
 
-<style scoped lang="less">
+<style scoped>
+.news {
+    &-inner {
+        &-background {
+            @apply relative;
+
+            &-image {
+                @apply w-dvw aspect-16-6 bg-fixed bg-no-repeat bg-cover bg-center;
+            }
+
+            &-gradient {
+                @apply absolute w-dvw z-10 h-60;
+
+                &-top {
+                    @apply top-0;
+                }
+
+                &-bottom {
+                    @apply bottom-0;
+                }
+            }
+
+            &-content {
+                @apply absolute z-20 flex flex-col justify-center items-center w-full h-full top-0;
+
+                &-title {
+                    h1 {
+                        @apply max-w-5xl uppercase text-center mb-2;
+                    }
+                }
+
+                &-description {
+                    h4 {
+                        @apply max-w-6xl text-center;
+                    }
+                }
+            }
+        }
+
+        &-content {
+            @apply z-10;
+        }
+    }
+}
+
+.dark-mode {
+    .news {
+        &-inner {
+            &-background {
+
+                &-image {
+                    @apply brightness-50;
+                }
+                
+                &-gradient {
+                    &-top {
+                        @apply bg-vlada-gradient-4;
+                    }
+
+                    &-bottom {
+                        @apply bg-vlada-gradient-3;
+                    }
+                }
+
+                &-content {
+                    &-title {
+                        h1 {
+                            @apply text-white;
+                        }
+                    }
+
+                    &-description {
+                        h4 {
+                            @apply text-white;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
 .fade-enter-active,
 .fade-leave-active {
     transition: opacity 0.5s ease;
