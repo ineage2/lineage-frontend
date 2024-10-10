f35488bc0073e98433e5ac4c78a38f57edfaec61
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-10-10 12:22:20 +0400

    fix share language change modal

--- components/common/share.vue
+++ components/common/share.vue
@@ -1,20 +1,17 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { useI18n } from 'vue-i18n';
+import {ref, onMounted} from 'vue';
+import {useI18n} from 'vue-i18n';
 import UiModalPrimary from '@/components/ui/modal.vue';
 
-const { setLocale } = useI18n();
+const {setLocale} = useI18n();
 const languageRef = ref(null);
 const colorMode = useColorMode();
 
-// Создаем локальную переменную для управления темой
 const themePreference = ref('system');
 
 onMounted(() => {
-    // Инициализируем локальную переменную значением из colorMode только на клиенте
     themePreference.value = colorMode.preference;
 
-    // Обновляем colorMode на основании локального состояния при необходимости
     colorMode.preference = themePreference.value;
 
     console.log(colorMode.preference);
@@ -24,7 +21,6 @@ const openModal = () => {
     languageRef.value.openModal();
 };
 
-// Обновляем цветовую тему
 const changeTheme = (newPreference) => {
     themePreference.value = newPreference;
     colorMode.preference = newPreference;
@@ -55,8 +51,10 @@ const changeTheme = (newPreference) => {
                     <ui-button-secondary @click="openModal" icon="bi:globe-americas"/>
                 </li>
                 <li data-aos="fade-left" data-aos-duration="750" data-aos-delay="300">
-                    <ui-button-secondary v-if="themePreference === 'system'" icon="bi:laptop-fill" @click="changeTheme('light')"/>
-                    <ui-button-secondary v-else-if="themePreference === 'light'" icon="bi:sun-fill" @click="changeTheme('dark')"/>
+                    <ui-button-secondary v-if="themePreference === 'system'" icon="bi:laptop-fill"
+                                         @click="changeTheme('light')"/>
+                    <ui-button-secondary v-else-if="themePreference === 'light'" icon="bi:sun-fill"
+                                         @click="changeTheme('dark')"/>
                     <ui-button-secondary v-else icon="bi:moon-fill" @click="changeTheme('system')"/>
                 </li>
             </ul>
@@ -67,19 +65,23 @@ const changeTheme = (newPreference) => {
         <section class="modal">
             <div class="download-modal-container">
                 <button @click="setLocale('en')">
-                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/united-kingdom-flag-icon.png"/>
+                    <nuxt-img class="download-modal-container-button-image"
+                              src="/img/flags/united-kingdom-flag-icon.png" loading="lazy"/>
                     {{ $t('languages.english') }} / {{ $t('language-codes.english') }}
                 </button>
                 <button @click="setLocale('ja')">
-                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/japan-flag-icon.png"/>
+                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/japan-flag-icon.png"
+                              loading="lazy"/>
                     {{ $t('languages.japanese') }} / {{ $t('language-codes.japanese') }}
                 </button>
                 <button @click="setLocale('ru')">
-                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/russia-flag-icon.png"/>
+                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/russia-flag-icon.png"
+                              loading="lazy"/>
                     {{ $t('languages.russian') }} / {{ $t('language-codes.russian') }}
                 </button>
                 <button @click="setLocale('zh')">
-                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/china-flag-icon.png"/>
+                    <nuxt-img class="download-modal-container-button-image" src="/img/flags/china-flag-icon.png"
+                              loading="lazy"/>
                     {{ $t('languages.chinese') }} / {{ $t('language-codes.chinese') }}
                 </button>
             </div>
@@ -87,36 +89,61 @@ const changeTheme = (newPreference) => {
     </ui-modal-primary>
 </template>
 
-<style scoped lang="less">
+<style scoped>
+.modal {
+    .download-modal-container {
+        @apply w-full flex flex-col;
+
+        button {
+            @apply w-full flex gap-3 mb-2 font-arpona-regular px-2 py-1 rounded;
+
+            .download-modal-container-button-image {
+                @apply w-6 h-6;
+            }
+        }
+    }
+}
+
+.dark-mode {
+    .modal {
+        .download-modal-container {
+            button {
+                @apply bg-vlada-color-6;
+            }
+        }
+    }
+}
+
+.light-mode {
+    .modal {
+        .download-modal-container {
+            button {
+                @apply bg-slate-200;
+            }
+        }
+    }
+}
+
+
 .fixed-panel {
-@apply fixed top-0 z-20 h-dvh;
+    @apply fixed top-0 z-20 h-dvh;
 
     &-container {
-    @apply px-5 py-4 h-full flex gap-4;
+        @apply px-5 py-4 h-full flex gap-4;
 
         &-list {
-        @apply flex flex-col gap-7 items-center justify-center;
+            @apply flex flex-col gap-7 items-center justify-center;
         }
     }
 }
 
 .left {
-@apply left-0;
+    @apply left-0;
 }
 
 .right {
-@apply right-0;
+    @apply right-0;
 }
 
-.download-modal-container {
-@apply w-full flex flex-col;
 
-    button {
-    @apply w-full flex gap-3 mb-2 font-arpona-regular px-2 py-1 rounded;
-
-        .download-modal-container-button-image {
-        @apply w-6 h-6;
-        }
-    }
-}
 </style>
